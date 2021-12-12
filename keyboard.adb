4df1e6e5244b3aa1c78e3bb3e2a7af9455351d1b
SMerrony <merrony@gmail.com>
2021-12-12 15:36:36 +0100

    Handle all keys mapped in DasherG

--- keyboard.adb
+++ keyboard.adb
@@ -58,6 +58,15 @@ package body Keyboard is
       end case;
    end Route_Key;
 
+   function Modify (B : in Byte) return Byte is
+      MB : byte := B;
+   begin
+      if Shift_Pressed then MB := MB - 16; end if;
+      if Ctrl_Pressed  then MB := MB - 64; end if;
+      return MB;
+   end Modify;
+
+   -- Handle_Key_Release maps PC-style keys to DASHER ones.
    procedure Handle_Key_Release (Key  : in Gdk_Key_Type) is
       Char_Byte : Byte;
    begin
@@ -67,10 +76,35 @@ package body Keyboard is
          when GDK_Shift_L | GDK_Shift_R => Shift_Pressed := False;
 
          when GDK_Return => Route_Key (Dasher_NL); -- convert PC-style Return to DG NL
+
+         when GDK_Tab    => Route_Key (Dasher_Tab);
          when GDK_Down   => Route_Key (Dasher_Cursor_Down);
          when GDK_Up     => Route_Key (Dasher_Cursor_Up);
          when GDK_Left   => Route_Key (Dasher_Cursor_Left);
          when GDK_Right  => Route_Key (Dasher_Cursor_Right);
+         when GDK_Home   => Route_Key (Dasher_Home);
+
+         -- The DEL key must map to 127 which is the DASHER DEL code
+         when GDK_Delete => Route_Key (Dasher_Delete); 
+
+         when GDK_Escape => Route_Key (Dasher_Escape);
+
+         when GDK_F1  => Route_Key (Dasher_Command); Route_Key (Modify (113));
+         when GDK_F2  => Route_Key (Dasher_Command); Route_Key (Modify (114));
+         when GDK_F3  => Route_Key (Dasher_Command); Route_Key (Modify (115));
+         when GDK_F4  => Route_Key (Dasher_Command); Route_Key (Modify (116));
+         when GDK_F5  => Route_Key (Dasher_Command); Route_Key (Modify (117));
+         when GDK_F6  => Route_Key (Dasher_Command); Route_Key (Modify (118));
+         when GDK_F7  => Route_Key (Dasher_Command); Route_Key (Modify (119));
+         when GDK_F8  => Route_Key (Dasher_Command); Route_Key (Modify (120));
+         when GDK_F9  => Route_Key (Dasher_Command); Route_Key (Modify (121));
+         when GDK_F10 => Route_Key (Dasher_Command); Route_Key (Modify (133));
+         when GDK_F11 => Route_Key (Dasher_Command); Route_Key (Modify (123));
+         when GDK_F12 => Route_Key (Dasher_Command); Route_Key (Modify (124));
+         when GDK_F13 => Route_Key (Dasher_Command); Route_Key (Modify (125));
+         when GDK_F14 => Route_Key (Dasher_Command); Route_Key (Modify (126));
+         when GDK_F15 => Route_Key (Dasher_Command); Route_Key (Modify (112));
+
          when others =>
             if Key < 256 then
                Char_Byte := Byte(Key);
--- terminal.ads
+++ terminal.ads
@@ -26,39 +26,55 @@ package Terminal is
    type Byte_Arr is array (Positive range <>) of Byte;
 
    type Terminal_T is tagged record
-      Emulation : Emulation_T;
-      Connection : Connection_T;
-      Cursor_X, Cursor_Y : Natural;
+      Emulation                        : Emulation_T;
+      Connection                       : Connection_T;
+      Cursor_X, Cursor_Y               : Natural;
       Roll_Enabled, Protection_Enabled : Boolean;
-      Holding, Logging, Scrolled_Back : Boolean;
-      Skip_Byte : Boolean;
-      Expecting : Boolean;
+      Holding, Logging, Scrolled_Back  : Boolean;
+      Skip_Byte                        : Boolean;
+      Expecting                        : Boolean;
       Raw_Mode : Boolean; -- in rawMode all host data is passed straight through to rawChan
       -- Selection_Region...
       -- Log_File : File;
       -- Display : Display_Acc_T;
       Blinking, Dimmed, Reversed, Underscored, Protectd : Boolean;
-      Updated : Boolean;
+      Updated                                           : Boolean;
    end record;
 
    type Terminal_Acc_T is access all Terminal_T;
 
-   Dasher_Rev_Off   : constant Byte := 2; -- from D210 onwards
-   Dasher_Blink_Enable  : constant Byte := 3; -- for the whole screen
-   Dasher_Blink_Disable : constant Byte := 4; -- for the whole screen
-   Dasher_NL        : constant Byte := 10;
-   Dasher_CR        : constant Byte := 13;
-   Dasher_Blink_On  : constant Byte := 14;
-   Dasher_Blink_Off : constant Byte := 15;
-   Dasher_Underline : constant Byte := 20;
-   Dasher_Normal    : constant Byte := 21; -- cancels Underline
-   Dasher_Rev_On    : constant Byte := 22; -- from D210 onwards
-   Dasher_Cursor_Up : constant Byte := 23;
-   Dasher_Cursor_Right : constant Byte := 24;
-   Dasher_Cursor_Left : constant Byte := 25;
-   Dasher_Cursor_Down : constant Byte := 26;
-   Dasher_Dim_On    : constant Byte := 28;
-   Dasher_Dim_Off   : constant Byte := 29;
+   Dasher_Print_Form       : constant Byte := 1;
+   Dasher_Rev_Off          : constant Byte := 2; -- from D210 onwards
+   Dasher_Blink_Enable     : constant Byte := 3; -- for the whole screen
+   Dasher_Blink_Disable    : constant Byte := 4; -- for the whole screen
+   Dasher_Read_Window_Addr : constant Byte := 5; -- requires response...
+   Dasher_Ack              : constant Byte := 6; -- sent to host to indicatelocal print is complete
+   Dasher_Bell             : constant Byte := 7;
+   Dasher_Home             : constant Byte := 8; -- window home
+   Dasher_Tab              : constant Byte := 9;
+   Dasher_NL               : constant Byte := 10;
+   Dasher_Erase_EOL        : constant Byte := 11;
+   Dasher_Erase_Page       : constant Byte := 12;
+   Dasher_CR               : constant Byte := 13;
+   Dasher_Blink_On         : constant Byte := 14;
+   Dasher_Blink_Off        : constant Byte := 15;
+   Dasher_Write_Window_Addr: constant Byte := 16; -- followed by col then row
+	Dasher_Print_Screen     : constant Byte := 17;
+	Dasher_Roll_Enable      : constant Byte := 18;
+	Dasher_Roll_Disable     : constant Byte := 19;
+   Dasher_Underline        : constant Byte := 20;
+   Dasher_Normal           : constant Byte := 21; -- cancels Underline
+   Dasher_Rev_On           : constant Byte := 22; -- from D210 onwards
+   Dasher_Cursor_Up        : constant Byte := 23;
+   Dasher_Cursor_Right     : constant Byte := 24;
+   Dasher_Cursor_Left      : constant Byte := 25;
+   Dasher_Cursor_Down      : constant Byte := 26;
+   Dasher_Escape           : constant Byte := 27;
+   Dasher_Dim_On           : constant Byte := 28;
+   Dasher_Dim_Off          : constant Byte := 29;
+   Dasher_Command          : constant Byte := 30;
+
+   Dasher_Delete           : constant Byte := 127;
 
    function Create (Emul : in Emulation_T) return Terminal_Acc_T;
    procedure Self_Test (T : in out Terminal_T);
