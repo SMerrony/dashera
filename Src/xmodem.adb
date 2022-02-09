e7be7ec4ccd0bab3560d9ce340eb4acfcb8db9fd
SMerrony <merrony@gmail.com>
2022-02-09 17:33:29 +0100

    Implement X-modem Send_Block procedure

--- Src/xmodem.adb
+++ Src/xmodem.adb
@@ -1,4 +1,6 @@
--- Copyright (C)2022 Steve Merrony
+-- Copyright ©2022 Steve Merrony
+
+-- This file is a part of DasherA.
 
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
@@ -17,8 +19,20 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 -- THE SOFTWARE.
 
+with Ada.Text_IO;
+
+with Redirector; use Redirector;
 
 package body Xmodem is
+
+   function BA_To_String (BA : in Byte_Arr) return string is
+      Str : string (1..BA'Length);
+   begin
+      for Ix in Str'Range loop
+         Str(Ix) := Byte_To_Char (BA(Ix));
+      end loop;
+      return Str;
+   end BA_To_String;
 
    function CRC_16 (BA : in Byte_Arr) return Unsigned_16 is
       CRC : Unsigned_16 := 0;
@@ -59,5 +73,41 @@ package body Xmodem is
       return CRC;
    end CRC_16_Fixed_Len;
 
+   procedure Send_Block (Data : in Byte_Arr; Block_Num : in Natural; Block_Size : in Packet_Size) is
+      Start_Bytes : string (1..3);
+      Block_Pos : constant Unsigned_8  := Unsigned_8(Block_Num mod 256);
+      Block_Inv : constant Unsigned_8  := not Block_Pos;
+      CRC       : constant Unsigned_16 := CRC_16_Fixed_Len (Data, Block_Size'Enum_Rep);
+      CRC_Str   : String (1..2);
+      Padding_String : string (1..1);
+   begin
+      if Block_Size = Short then
+         Start_Bytes(1) := Ascii.SOH;
+      else 
+         Start_Bytes(1) := Ascii.STX;
+      end if;
+      Start_Bytes(2) := Character'Val(Block_Pos);
+      Start_Bytes(3) := Character'Val(Block_Inv);
+      if Tracing then
+         Ada.Text_IO.Put_Line ("DEBUG: X-Modem sending start byte and block number: " & Block_Pos'Image);
+      end if;
+      Router.Send_Data (Start_Bytes);
+
+      -- Send the actual data
+      Router.Send_Data (BA_To_String (Data));
+
+      -- Pad out block
+      if Data'Length < Block_Size'Enum_Rep then
+         Padding_String(1) := Ascii.EOT;
+         for Ix in Data'Length .. Block_Size'Enum_Rep loop
+            Router.Send_Data (Padding_String);
+         end loop;
+      end if;
+
+      CRC_Str(1) := Byte_To_Char ( Unsigned_8(Shift_Right (CRC, 8)));
+      CRC_Str(2) := Byte_To_Char ( Unsigned_8(CRC and 16#00ff#));
+      Router.Send_Data (CRC_Str);
+
+   end Send_Block;
 
 end Xmodem;
--- Src/xmodem.ads
+++ Src/xmodem.ads
@@ -17,11 +17,14 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 -- THE SOFTWARE.
 
+with Ada.Unchecked_Conversion;
+
 with Interfaces;  use Interfaces;
 
 package Xmodem is
 
    type Packet_Size is (Short, Long);
+   for Packet_Size use (Short => 128, Long => 1024);
 
    -- procedure Receive;
    -- procedure Send ( Pkt_Len : in Packet_Size);
@@ -30,12 +33,20 @@ private
 
    type Byte_Arr is array (Positive range<>) of Unsigned_8;
 
+
+   function Char_To_Byte        is new Ada.Unchecked_Conversion(Character, Unsigned_8);
+   function Byte_To_Char        is new Ada.Unchecked_Conversion(Unsigned_8, Character);
+
+   function BA_To_String (BA : in Byte_Arr) return string;
+   
    function CRC_16 (BA : in Byte_Arr) return Unsigned_16;
    -- Calculate the CRC-16 value of the provided block of data
 
    function CRC_16_Fixed_Len (BA : in Byte_Arr; FL : in Positive) return Unsigned_16;
    -- Calculate the CRC-16 Constant for the provided block of data
 
-   -- procedure Send_Block (Blk_Num : in Natural);
+   procedure Send_Block (Data : in Byte_Arr; Block_Num : in Natural; Block_Size : in Packet_Size);
+
+   Tracing : Boolean;
 
 end Xmodem;
